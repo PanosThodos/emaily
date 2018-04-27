d7db1db40245b481087327ffde165778b8f41732
PanosThodos <panosthodos@gmail.com>
2018-04-28 00:56:17 +0300

    client side configs for servers

--- services/passport.js
+++ services/passport.js
@@ -3,7 +3,7 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const mongoose = require("mongoose");
 const keys = require("../config/keys");
 
-const User = mongoose.model("users"); 
+const User = mongoose.model("users");
 //Creates User object (instead of require)
 
 passport.serializeUser((user, done) => {
@@ -11,9 +11,8 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((id, done) => {
-	User.findById(id)
-		.then(user => {
-			done(null, user);
+	User.findById(id).then(user => {
+		done(null, user);
 	});
 });
 
@@ -25,17 +24,12 @@ passport.use(
 			callbackURL: "/auth/google/callback",
 			proxy: true
 		},
-		(accessToken, refreshToken, profile, done) => {
-			User.findOne({ googleID: profile.id }).then(existingUser => {
-				if (existingUser) {
-					//we already have a record with the given profileID
-					done(null, existingUser);
-				} else {
-					//we don't have a user record with this ID, make a new record in mongo
-					new User({ googleID: profile.id }).save()//User object(which is actually a User collection in mongodb) creates instance googleID and registers the profileID of google oauth then saves it to mognodb
-					.then(user => done(null, user));
-				}
-			});
+		async (accessToken, refreshToken, profile, done) => {
+			const existingUser = await User.findOne({ googleID: profile.id });
+			if (existingUser) {
+				return done(null, existingUser);
+			}
+			const user = await new User({ googleID: profile.id }).save();
 		}
 	)
 );
